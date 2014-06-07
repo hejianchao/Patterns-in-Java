daa928a7fd59055fdbc64b71ee2c802ee9961c29
chinan.hjc <chinan.hjc@taobao.com>
2014-06-08 03:25:51 +0800

    modify it for test

--- src/main/java/observer_pattern/Client.java
+++ src/main/java/observer_pattern/Client.java
@@ -6,15 +6,15 @@ public class Client {
 		HouseZhongjie zj = new HouseZhongjie();
 		FangKe fk1 = new FangKe("小何");
 		FangKe fk2 = new FangKe("小刘");
-		
+
 		zj.addObserver(fk1);
 		zj.addObserver(fk2);
-		
+
 		zj.setChanged();
 		zj.notifyObservers("房子降价了哦。。");
 		System.out.println(zj.countObservers());
 		System.out.println(zj.hasChanged());
-		System.out.println("----");
+		System.out.println("--------");
 	}
 
 }
